1dce29ea8316903f52cea1c86f1ef47b1257e54d
Ruan Molina <ruan.molina@hotmail.com>
2024-10-07 19:05:16 -0300

    Test cidade create added

---
+++ test/cidade/create.test.ts
@@ -0,0 +1,20 @@
+import { StatusCodes } from "http-status-codes";
+import { testServer } from "../jest.setup";
+
+describe('Cidade - create', ()=>{
+
+    it('Criar um registro -sucesso',async ()  => {
+
+        const resp = await testServer.post('/').send({name:'cidade 1'});
+
+        expect(StatusCodes.CREATED).toEqual(resp.statusCode);
+    });
+    it('Criar um registro - fracasso - falta de caracteres',async ()  => {
+
+        const resp = await testServer.post('/').send({name:'JU'});
+
+        expect(StatusCodes.BAD_REQUEST).toEqual(resp.statusCode);
+        console.log(resp.body)
+        expect(resp.body).toHaveProperty('errors.body.name');
+    });
+});
